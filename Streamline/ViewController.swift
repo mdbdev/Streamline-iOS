c7ad7ab3a94d54d44179b083fbce090610383f86
Stephen Jayakar <ajay@airbears2-10-142-37-182.airbears2.1918.berkeley.edu>
2017-10-14 13:29:28 -0700

    First commit

---
+++ Streamline/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Streamline
+//
+//  Created by Stephen Jayakar on 10/14/17.
+//  Copyright © 2017 Stephen Jayakar. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
