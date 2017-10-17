f6e6acf5d130187f297d38e5ee1e2659e74d33f5
Vineeth Yeevani <vineethyeevani@Vineeths-MacBook-Pro.local>
2017-10-16 19:46:59 -0700

    Added database Util to check if user exists

--- Streamline/Utils/DB.swift
+++ Streamline/Utils/DB.swift
@@ -18,6 +18,22 @@ class DB {
         reference.child("users").child(username).setValue(userData)
     }
     
+    static func checkUserExists(username: String) -> Bool {
+        var userExists = false
+        let reference = Database.database().reference().child("users")
+        reference.observeSingleEvent(of: .value) { (snapshot, error) in
+            if error != nil {
+                print(error)
+            } else {
+                if snapshot.hasChild(username) {
+                    print("Logged in User has used app before and exists in database with username \(username)")
+                    userExists = true
+                }
+            }
+        }
+        return userExists
+    }
+    
     // create post with post class file and user
     // Probably pass in post to with block?
 }
