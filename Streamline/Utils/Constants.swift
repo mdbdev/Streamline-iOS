b742f94a89a84f827def9589edaaa5afe8c68b01
Stephen Jayakar <ajay@Stephens-MacBook-Pro.local>
2017-10-19 21:00:17 -0700

    some organizational changes

---
+++ Streamline/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Streamline
+//
+//  Created by Stephen Jayakar on 10/19/17.
+//  Copyright © 2017 Stephen Jayakar. All rights reserved.
+//
+
+
+struct Constants {
+    static let darkPurple = UIColor(hex: "673AB7")
+}
