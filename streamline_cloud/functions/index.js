b53957c2cfbdbdf7f115f25fc8de9712ea5e53e3
Vineeth Yeevani <vineethyeevani@airbears2-10-142-93-226.airbears2.1918.berkeley.edu>
2017-10-28 01:54:19 -0700

    autologin completed and DB function withBlock

--- streamline_cloud/functions/index.js
+++ streamline_cloud/functions/index.js
@@ -5,20 +5,28 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+exports.test = functions.https.onRequest((req, res) => {
+  console.log("test");
+  return res.status(200).send("testytesty");
+});
+
 //Waits for cron job from cronjob.org request and then checks the Posts data
 exports.deletePosts = functions.https.onRequest((req, res) => {
-  var ref = admin.database().ref('/posts/{pushId}');
+  var ref = admin.database().ref('/posts/{pushId}/');
   var now = Date.now();
-  console.log(now);
   var cutoff = now - 12 * 60 * 60 * 1000;
-  var oldItemsQuery = ref.orderByChild('timestamp').endAt(cutoff);
+  var oldItemsQuery = ref.orderByChild('timePosted').endAt(cutoff);
+  console.log("test");
+  console.log(cutoff);
   return oldItemsQuery.once('value', function(snapshot) {
     // create a map with all children that need to be removed
     var updates = {};
     snapshot.forEach(function(child) {
+      console.log(child.key);
       updates[child.key] = null
     });
     // execute all updates in one go and return the result to end the function
     return ref.update(updates);
+    res.status(200).send("test");
   });
 });
