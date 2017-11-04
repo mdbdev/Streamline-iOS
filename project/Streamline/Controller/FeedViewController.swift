bbe5296f79afb53d4791827b91814b798152b032
Stephen Jayakar <aj12ay@gmail.com>
2017-11-04 16:33:54 -0700

    feed mvc

--- project/Streamline/Controller/FeedViewController.swift
+++ project/Streamline/Controller/FeedViewController.swift
@@ -13,14 +13,8 @@ import AVFoundation
 
 class FeedViewController: UIViewController {
     var modalView: AKModalView!
-    var postCollectionView: UICollectionView!
-    var postButton: UIButton!
-    var logoutButton: UIButton!
-    var discoverLabel: UILabel!
-    var nowPlayingButton: UIButton!
-    var nowPlayingLabel: UILabel!
-    var nowPlayingArtist: UILabel!
-    var nowPlayingImage: UIImageView!
+    var subView: FeedView!
+    
     var nowPlayingVC: NowPlayingViewController!
     var searchView: SearchView!
     
@@ -28,7 +22,12 @@ class FeedViewController: UIViewController {
     var refHandle: DatabaseReference!
     
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
+        subView = FeedView(frame: view.frame)
+        subView.delegate = self
+        view.addSubview(subView)
+        subView.postCollectionView.delegate = self
+        subView.postCollectionView.dataSource = self
         // Preloading the player view
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         nowPlayingVC = storyboard.instantiateViewController(withIdentifier: "NowPlayingViewController") as! NowPlayingViewController
@@ -36,11 +35,10 @@ class FeedViewController: UIViewController {
         self.refHandle = Database.database().reference()
         self.refHandle.observe(DataEventType.value, with: { (snapshot) in
             DB.getPosts(withBlock : {
-                self.postCollectionView.reloadData()
+                self.subView.postCollectionView.reloadData()
             })
             
         })
-        setupUI()
         
         setupSpotify()
         
@@ -52,7 +50,7 @@ class FeedViewController: UIViewController {
     
     // TODO: Change the now playing index to match the new data!
     func populateFeed() {
-        postCollectionView.reloadData()
+        subView.postCollectionView.reloadData()
     }
     
     // Setup Functions
@@ -67,46 +65,13 @@ class FeedViewController: UIViewController {
         }
     }
     
-    // Selectors
-    func postButtonPressed() {
-        searchView = SearchView(frame: Utils.rRect(rx: 40, ry: 152, rw: 295, rh: 289), large: true)
-        searchView.layer.cornerRadius = 20
-        searchView.delegate = self
-        //self.performSegue(withIdentifier: "toNewPost", sender: self)
-        DB.currentUser.getPID {
-            //if DB.currentUser.pid == "" {
-                self.createSearchView()
-            //} else {
-                //TODO: Check if song is past deadline
-                //let post =
-            //}
-            
-        }
-        
-    }
-    
     func createSearchView(){
         modalView = AKModalView(view: searchView)
         modalView.automaticallyCenter = false
         view.addSubview(modalView)
         modalView.show()
     }
-    
-    func logoutButtonPressed() {
-        
-        
-        let userDefaults = UserDefaults.standard
-        userDefaults.removeObject(forKey: "SpotifySession")
-        
-        SpotifyAPI.player.logout()
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    func nowPlayingButtonPressed() {
-        if let index = State.nowPlayingIndex {
-            self.present(nowPlayingVC, animated: true, completion: nil)
-        }
-    }
+
 }
 
 extension FeedViewController: SearchViewDelegate {
@@ -160,23 +125,19 @@ extension FeedViewController: UICollectionViewDelegate, UICollectionViewDataSour
     
     func changeLabel(post: Post, index: Int) {
         let post = DB.posts[index]
-        nowPlayingLabel.text = post.songTitle //post.songTitle
-        nowPlayingArtist.text = post.artist
+        subView.nowPlayingLabel.text = post.songTitle //post.songTitle
+        subView.nowPlayingArtist.text = post.artist
         // TODO: Make it in the cell
-        let cell = postCollectionView.cellForItem(at: IndexPath(item: index, section: 0)) as! PostCollectionViewCell
-        nowPlayingImage.image = cell.albumImage.image
+        let cell = subView.postCollectionView.cellForItem(at: IndexPath(item: index, section: 0)) as! PostCollectionViewCell
+        subView.nowPlayingImage.image = cell.albumImage.image
     }
 }
 
+// Spotify Extension
 extension FeedViewController: SPTAudioStreamingDelegate, SPTAudioStreamingPlaybackDelegate {
     func activateAudioSession() {
         try? AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
         try? AVAudioSession.sharedInstance().setActive(true)
-    }
-    
-    func audioStreamingDidSkip(toNextTrack audioStreaming: SPTAudioStreamingController!) {
-        print("awlekfjawelkfjawe;lfjkawe;flkajwef")
-        self.nowPlayingLabel.text = audioStreaming.metadata.currentTrack?.albumName
     }
     
     func audioStreaming(_ audioStreaming: SPTAudioStreamingController!, didStopPlayingTrack trackUri: String!) {
@@ -196,8 +157,34 @@ extension FeedViewController: SPTAudioStreamingDelegate, SPTAudioStreamingPlayba
     }
 }
 
-extension FeedViewController: NowPlayingProtocol {
+extension FeedViewController: NowPlayingProtocol, FeedViewDelegate {
     func passLabel(post: Post, index: Int) {
         changeLabel(post: post, index: index)
+    }
+    
+    // Selectors
+    func postButtonPressed() {
+        searchView = SearchView(frame: Utils.rRect(rx: 40, ry: 152, rw: 295, rh: 289), large: true)
+        searchView.layer.cornerRadius = 20
+        searchView.delegate = self
+        //self.performSegue(withIdentifier: "toNewPost", sender: self)
+        DB.currentUser.getPID {
+            //if DB.currentUser.pid == "" {
+            self.createSearchView()
+        }
+    }
+    
+    func nowPlayingButtonPressed() {
+        if let index = State.nowPlayingIndex {
+            self.present(nowPlayingVC, animated: true, completion: nil)
+        }
+    }
+    
+    func logoutButtonPressed() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.removeObject(forKey: "SpotifySession")
+        
+        SpotifyAPI.player.logout()
+        self.dismiss(animated: true, completion: nil)
     }
 }
--- project/Streamline/View/FeedView.swift
+++ project/Streamline/View/FeedView.swift
@@ -8,7 +8,21 @@
 
 
 // TODO: Rewrite these to actual views rather than extensions
-extension FeedViewController {    
+class FeedView: UIView {
+    var postCollectionView: UICollectionView!
+    var postButton: UIButton!
+    var logoutButton: UIButton!
+    var discoverLabel: UILabel!
+    var nowPlayingButton: UIButton!
+    var nowPlayingLabel: UILabel!
+    var nowPlayingArtist: UILabel!
+    var nowPlayingImage: UIImageView!
+    var delegate: FeedViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
     
     func setupUI() {
         setupCollectionView()
@@ -22,30 +36,30 @@ extension FeedViewController {
         nowPlayingButton = UIButton(frame: Utils.rRect(rx: 0, ry: 609, rw: 375, rh: 60))
         nowPlayingButton.backgroundColor = UIColor.white
         nowPlayingButton.addTarget(self, action: #selector(nowPlayingButtonPressed), for: .touchUpInside)
-        view.addSubview(nowPlayingButton)
+        addSubview(nowPlayingButton)
         
         nowPlayingLabel = UILabel(frame: Utils.rRect(rx: 61, ry: 617, rw: 268, rh: 28))
         nowPlayingLabel.textColor = UIColor.black
 
         nowPlayingLabel.adjustsFontSizeToFitWidth = true
         nowPlayingLabel.font = Constants.averageSans?.withSize(25)
-        view.addSubview(nowPlayingLabel)
+        addSubview(nowPlayingLabel)
         
         nowPlayingArtist = UILabel(frame: Utils.rRect(rx: 61, ry: 644, rw: 268, rh: 16))
         nowPlayingArtist.textColor = UIColor.black
         nowPlayingArtist.adjustsFontSizeToFitWidth = true
         nowPlayingArtist.font = Constants.averageSans?.withSize(15)
-        view.addSubview(nowPlayingArtist)
+        addSubview(nowPlayingArtist)
         
         // TODO: Use annie's square function
         nowPlayingImage = UIImageView(frame: Utils.rRect(rx: 8, ry: 617, rw: 47, rh: 47))
         nowPlayingImage.image = UIImage(named: "albumPlaceholder")
         nowPlayingImage.contentMode = .scaleAspectFit
-        view.addSubview(nowPlayingImage)
+        addSubview(nowPlayingImage)
     }
     
     func setupBackground() {
-        view.backgroundColor = Constants.cvBackground
+        backgroundColor = Constants.cvBackground
     }
     
     func setupCollectionView() {
@@ -55,10 +69,9 @@ extension FeedViewController {
         layout.minimumInteritemSpacing = 8
         postCollectionView = UICollectionView(frame: Utils.rRect(rx: 21, ry: 69, rw: 334, rh: 541), collectionViewLayout: layout)
         postCollectionView.register(PostCollectionViewCell.self, forCellWithReuseIdentifier: "postCell")
-        postCollectionView.delegate = self
-        postCollectionView.dataSource = self
+
         postCollectionView.backgroundColor = Constants.cvBackground
-        view.addSubview(postCollectionView)
+        addSubview(postCollectionView)
     }
     
     func setupButton() {
@@ -74,14 +87,14 @@ extension FeedViewController {
         postButton.layer.borderColor = UIColor(hex: "311b92").cgColor
         
         postButton.addTarget(self, action: #selector(postButtonPressed), for: .touchUpInside)
-        view.addSubview(postButton)
+        addSubview(postButton)
         
         logoutButton = UIButton(frame: Utils.rRect(rx: 15, ry: 30, rw: 74, rh: 22))
         logoutButton.setTitle("Log Out", for: .normal)
         logoutButton.setTitleColor(UIColor(hex: "737171"), for: .normal)
         logoutButton.addTarget(self, action: #selector(logoutButtonPressed), for: .touchUpInside)
         logoutButton.titleLabel?.adjustsFontSizeToFitWidth = true
-        view.addSubview(logoutButton)
+        addSubview(logoutButton)
     }
     
     func setupLabel() {
@@ -95,6 +108,29 @@ extension FeedViewController {
         discoverLabel.font = Constants.averageSans?.withSize(20)
         discoverLabel.font = UIFont.systemFont(ofSize: 30)
         
-        view.addSubview(discoverLabel)
+        addSubview(discoverLabel)
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Selectors
+    func postButtonPressed() {
+        self.delegate?.postButtonPressed()
+    }
+    
+    func nowPlayingButtonPressed() {
+        self.delegate?.nowPlayingButtonPressed()
+    }
+    
+    func logoutButtonPressed() {
+        self.delegate?.logoutButtonPressed()
+    }
+}
+
+protocol FeedViewDelegate {
+    func postButtonPressed()
+    func nowPlayingButtonPressed()
+    func logoutButtonPressed()
 }
